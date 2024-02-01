a74bcac4621a733e2073ff960d600159578010c5
Andres Montealegre <andres.montealegre@oracle.com>
2024-02-01 14:10:29 -0500

    removes module composition

--- main.tf
+++ main.tf
@@ -44,30 +44,3 @@ resource "oci_containerengine_cluster" "main" {
     service_lb_subnet_ids = [local.lb_subnet_id]
   }
 }
-
-module "node_pool" {
-  source = "git@github.com:andresmonteal/terraform-oci-oke-node-pool.git?ref=v0.1.0"
-
-  for_each = var.node_pool
-
-  cluster_id      = oci_containerengine_cluster.main.id
-  tenancy_ocid    = var.tenancy_ocid
-  compartment_id  = local.oke_cmp_id
-  node_pool_name  = each.key
-  node_pool_shape = each.value["shape"]
-
-  defined_tags                 = var.defined_tags
-  freeform_tags                = local.merged_freeform_tags
-  node_pool_kubernetes_version = var.cluster_kubernetes_version
-  ssh_public_key               = base64decode(data.oci_secrets_secretbundle.bundle[each.key].secret_bundle_content.0.content)
-
-  shape_memory = each.value["memory"]
-  shape_ocpus  = each.value["ocpu"]
-
-  subnet_id = data.oci_core_subnets.wrkr_subnet[each.key].subnets[0].id
-
-  placement_configs = each.value["placement_configs"]
-  node_pool_size    = each.value["size"]
-
-  node_pool_image_id = each.value["image_id"]
-}
